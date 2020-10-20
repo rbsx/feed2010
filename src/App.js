a84ed748e2881da201bd0054cb35ba1e346d82f9
rbsx <rbsx@users.noreply.github.com>
2020-10-20 18:19:48 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useLayoutEffect } from "react";
 import Tweet from "./Components/Tweet";
 import "./styles.css";
 import { fetchTweets } from "./Utils/fetch";
@@ -7,8 +7,63 @@ import { TWEETS_AMOUNT } from "./constants";
 
 export default function App() {
   const [tweets, setTweets] = useState([]);
+  const [position, setPosition] = useState(0);
+  const [bottom, setBottom] = useState(false);
   const [lastTweetId, setLastTweetId] = useState(null);
+  const [firstTweetId, setFirstTweetId] = useState(null);
   const [isFetching, setIsFetching] = useState(false);
+
+  useLayoutEffect(() => {
+    function updatePosition() {
+      setPosition(window.pageYOffset);
+
+      const windowHeight =
+        "innerHeight" in window
+          ? window.innerHeight
+          : document.documentElement.offsetHeight;
+      const body = document.body;
+      const html = document.documentElement;
+      const docHeight = Math.max(
+        body.scrollHeight,
+        body.offsetHeight,
+        html.clientHeight,
+        html.scrollHeight,
+        html.offsetHeight
+      );
+      const windowBottom = windowHeight + window.pageYOffset;
+      if (windowBottom >= docHeight) {
+        setBottom(true);
+      } else {
+        setBottom(false);
+      }
+    }
+    window.addEventListener("scroll", updatePosition);
+    updatePosition();
+    return () => window.removeEventListener("scroll", updatePosition);
+  }, []);
+
+  const handleOlderTweets = () => {
+    fetchTweets(TWEETS_AMOUNT, null, firstTweetId).then((newTweets) => {
+      if (newTweets.length === 0) {
+        console.log("Refetch after error");
+        handleOlderTweets();
+      } else {
+        let oldTweets = cloneDeep(tweets);
+        if (newTweets.length > 0) {
+          setFirstTweetId(newTweets[newTweets.length - 1].id);
+        }
+        setTweets(oldTweets.concat(newTweets));
+        setIsFetching(false);
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (bottom && position > 0) {
+      setIsFetching(true);
+      handleOlderTweets();
+    }
+  }, [bottom]);
 
   const updateTweets = () => {
     fetchTweets(TWEETS_AMOUNT, lastTweetId).then((newTweets) => {
@@ -27,6 +82,10 @@ export default function App() {
       setTweets(tweets);
       if (tweets.length > 0) {
         setLastTweetId(tweets[0].id);
+
+        if (!firstTweetId) {
+          setFirstTweetId(tweets[tweets.length - 1].id);
+        }
       }
       setIsFetching(false);
     });
@@ -34,7 +93,7 @@ export default function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (!isFetching) {
+      if (!isFetching && position === 0) {
         setIsFetching(true);
         updateTweets();
       }
@@ -43,7 +102,7 @@ export default function App() {
     return () => {
       clearInterval(interval);
     };
-  }, [isFetching]);
+  }, [isFetching, position]);
 
   return (
     <div className="App">
